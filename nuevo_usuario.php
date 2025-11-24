3dc5b136ac6fb944f5efe21a565f8d01e537d205
tfermoso <tomas.fermoso@gmail.com>
2025-11-24 17:46:30 +0100

    creamos usuario

--- nuevo_usuario.php
+++ nuevo_usuario.php
@@ -1,14 +1,5 @@
 <?php
-/*
-if (isset($_COOKIE["usuario"])) {
-    $usuario = $_COOKIE["usuario"];
-} else {
-    header("Location: index.php");
-    exit();
-}
- */
 session_start();
-
 if (isset($_SESSION["usuario"])) {
     $usuario = $_SESSION["usuario"];
     $contador = $_SESSION["contador"][$usuario];
@@ -17,10 +8,33 @@ if (isset($_SESSION["usuario"])) {
     header("Location: index.php");
     exit();
 }
+if (isset($_POST["nuevo_usuario"])) {
+
+    $nuevo_usuario = trim($_POST["nuevo_usuario"]);
+    if ($nuevo_usuario !== "" && !isset($_SESSION["contador"][$nuevo_usuario])) {
+        $_SESSION["contador"][$nuevo_usuario] = 0;
+        header("Location: bienvenido.php");
+        exit();
+    } else {
+        $error = "El nombre de usuario ya existe.";
+    }
+}
+
 ?>
 <?php include_once './includes/cabecera.php'; ?>
 <seccion class="main">
-    
+    <form action="" method="post">
+        <h2>Nuevo Usuario</h2>
+        <label for="nuevo_usuario">Usuario:
+            <input type="text" name="nuevo_usuario" id="nuevo_usuario" required>
+        </label>
+        <br>
+        <input type="submit" value="Crear Usuario">
+    </form>
+    <p>
+        <?php if (isset($error))
+            echo $error; ?>
+    </p>
 </seccion>
 
 <?php include_once './includes/footer.php'; ?>
--- ver_usuario.php
+++ ver_usuario.php
@@ -12,7 +12,7 @@ session_start();
 if (isset($_SESSION["usuario"])) {
     $usuario = $_SESSION["usuario"];
     $contador = $_SESSION["contador"][$usuario];
-    $contadores = $_SESSION["contador"];
+  
 } else {
     header("Location: index.php");
     exit();
@@ -20,7 +20,11 @@ if (isset($_SESSION["usuario"])) {
 ?>
 <?php include_once './includes/cabecera.php'; ?>
 <seccion class="main">
-    
+    <label for="">Usuario:</label>
+    <span><?= htmlspecialchars($usuario) ?></span>
+    <br>
+    <label for="">Contador: </label>
+    <span><?= $contador ?></span>
 </seccion>
 
 <?php include_once './includes/footer.php'; ?>
